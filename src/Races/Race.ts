1e1500972af341ed2036458c074bb87c08fc6ac5
evertonpsantos <evertonpsantos8@gmail.com>
2023-01-18 15:59:38 -0300

    Cria a classe Race :elf:

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,1 +1,9 @@
-// VQV :rocket:
+export default abstract class Race {
+  constructor(private _name: string, private _dexterity: number) { }
+
+  get name(): string { return this._name; }
+  get dexterity(): number { return this._dexterity; }
+  abstract get maxLifePoints(): number;
+
+  static createdRacesInstances() { throw new Error('Not implemented'); }
+}
